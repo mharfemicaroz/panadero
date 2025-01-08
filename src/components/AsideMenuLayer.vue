f49632d0886b2fe91e140506f2c76e3199b7b8dd
unknown <mharfemicaroz@gmail.com>
2025-01-08 16:16:43 +0800

    changes

--- src/components/AsideMenuLayer.vue
+++ src/components/AsideMenuLayer.vue
@@ -33,7 +33,7 @@ const asideLgCloseClick = (event) => {
 <template>
   <aside
     id="aside"
-    class="lg:py-2 lg:pl-2 w-60 fixed flex z-40 top-0 h-screen transition-position overflow-hidden"
+    class="lg:py-2 lg:pl-2 w-100 fixed flex z-40 top-0 h-screen transition-position overflow-hidden"
   >
     <div class="aside lg:rounded-2xl flex-1 flex flex-col overflow-hidden dark:bg-slate-900">
       <div class="aside-brand flex flex-row h-14 items-center justify-between dark:bg-slate-900">
